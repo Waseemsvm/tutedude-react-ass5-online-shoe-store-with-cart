8a4d0395fe47fba517e395b4c4e7116694c7054e
Waseemsvm <waseemsvm14@gmail.com>
2025-07-21 21:50:44 +0530

    updated styles for homepage, contact and about us page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,10 +2,10 @@ import homepageStyles from "../styles/Homepage.module.css";
 export default function HomePage() {
   return (
     <div className={homepageStyles.homepage}>
-      <p className={homepageStyles["homepage-text"]}>
+      {/* <p className={homepageStyles["homepage-text"]}>
         <span>👣 Your Next Step Starts Here</span> <br /> Browse, buy, and walk
         in confidence.
-      </p>
+      </p> */}
     </div>
   );
 }
