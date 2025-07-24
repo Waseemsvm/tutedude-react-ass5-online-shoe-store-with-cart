0917312afdbb09f63e70e6e3cfe0ea6da4bdd468
Waseemsvm <waseemsvm14@gmail.com>
2025-07-24 21:18:48 +0530

    updated checkout page validation

--- src/components/MessageToast.jsx
+++ src/components/MessageToast.jsx
@@ -3,20 +3,27 @@ import { useMessageToast } from "./MessageToastContext";
 export default function MessageToast() {
   const { isMessageToastOpen, showMessageToast } = useMessageToast();
   return (
-    <div
-      className={`${MessageToastStyles["message-toast"]} ${
-        isMessageToastOpen ? "" : MessageToastStyles.hide
-      }`}
-    >
-      <h1>Order placed Successfully</h1>
-      <p>Your order reference number is #111222333</p>
-      <button
-        onClick={() => {
-          showMessageToast(false);
-        }}
+    <>
+      <div
+        className={`${MessageToastStyles["overlay"]} ${
+          isMessageToastOpen ? "" : MessageToastStyles.hide
+        }`}
+      ></div>
+      <div
+        className={`${MessageToastStyles["message-toast"]} ${
+          isMessageToastOpen ? "" : MessageToastStyles.hide
+        }`}
       >
-        Dismiss
-      </button>
-    </div>
+        <h1>Order placed Successfully</h1>
+        <p>Your order reference number is #111222333</p>
+        <button
+          onClick={() => {
+            showMessageToast(false);
+          }}
+        >
+          Dismiss
+        </button>
+      </div>
+    </>
   );
 }
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -2,12 +2,14 @@ import LayoutStyles from "../styles/Layout.module.css";
 import Navbar from "../components/Navbar";
 import Main from "../components/Main";
 import { connect } from "react-redux";
+import MessageToast from "../components/MessageToast";
 
 export default function Layout() {
   return (
     <div className={LayoutStyles.main}>
       <Navbar />
       <Main />
+      <MessageToast />
     </div>
   );
 }
